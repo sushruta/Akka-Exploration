a72b2bc4db3875793f9a41b6c2be39c28e51a241
Sashidhar Guntury <sguntury@gmail.com>
2017-11-21 11:09:13 -0500

    new versions of akka

--- genproj.sh
+++ genproj.sh
@@ -23,9 +23,9 @@ scalaVersion := \"2.11.8\"
 
 libraryDependencies ++= Seq(
   \"com.typesafe.akka\" %% \"akka-http\" % \"10.0.10\",
-  \"com.typesafe.akka\" %% \"akka-actor\" % \"2.5.4\",
-  \"com.typesafe.akka\" %% \"akka-stream\" % \"2.5.4\",
-  \"com.typesafe.akka\" %% \"akka-testkit\" % \"2.5.4\" % Test
+  \"com.typesafe.akka\" %% \"akka-actor\" % \"2.5.6\",
+  \"com.typesafe.akka\" %% \"akka-stream\" % \"2.5.6\",
+  \"com.typesafe.akka\" %% \"akka-testkit\" % \"2.5.6\" % Test
 )" > $PROJDIR/build.sbt
 
 echo "# general scala stuff
